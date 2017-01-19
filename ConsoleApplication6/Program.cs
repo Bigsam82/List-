18bf0be316f9146446bcbb2c3ea92c0993f09afa
Samuel Saunders <sam.saunders116@yahoo.com>
2017-01-19 14:28:53 -0500

    first commit

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  List<int> intlive = new List<int>();
+            // intlive.Add(19);
+            // intlive.Add(23);
+            // intlive.Add(55);
+            // intlive.Add(22);
+            // Console.WriteLine(intlive.Count);
+
+            // List<string> listsam = new List<string>();
+            // listsam.Add("one");
+            // listsam.Add("two");
+            // listsam.Add("three");
+            // listsam.Add("four");
+            // listsam.Add(null);
+            // Console.WriteLine(listsam.Count);
+
+            // List<int> samlist = new List<int>() { 10, 20, 22, 34, 50 };
+            //Console.WriteLine(samlist.Count);
+
+            //    List<string> stingList = new List<string>() { "Dolphins", "Dinosuars", "Bears", "Alligators", "Sloths" };
+
+            //   foreach (string allAnimals in stingList)//loop a string
+            // {
+            //     Console.WriteLine(allAnimals);
+            // }
+
+
+
+            // List<Student> studentList = new List<Student>() {
+            //     new Student () {StudentsID=1, StudentName="Bill"},
+            //    new Student () {StudentsID=2, StudentName="Sam" },
+            //new Student () {StudentsID=3, StudentName="Ronin"}
+            //   };
+            //   Console.WriteLine(studentList.Count);
+
+            // List<int> intMoney = new List<int>() { 2, 1, 3, 5, 6 };
+            //  Console.WriteLine(intMoney.Count);
+
+
+            // List<int> intList = new List<int>() { 10, 20, 17, 15 };
+            // int elem = intList[1];
+            // Console.WriteLine(intList.Count);
+
+            // string[] strArray = new string[2];
+            //strArray[0] = "Hello";
+            //strArray[1] = "World";
+            //Print(strArray);
+
+
+            // List<string> strList = new List<string>();
+            // strList.Add("Hello");
+            //strList.Add("World");
+            // Print(strList);
+
+
+            //List<int> list = new List<int>();
+            // list.Add(2);
+            // list.Add(4);
+            //  list.Add(5);
+            //  list.Add(0);
+            //  list.Add(21);
+            //  list.Add(16);
+            //   Console.WriteLine(list[3]);
+            //  Console.WriteLine(list.Count);
+            //  list.IndexOf(16);
+            //  list.RemoveAt(2);
+            //  Console.WriteLine(list[list.IndexOf(16)]);
+
+            // foreach (int coldValue in list)
+            //  {
+            //     Console.WriteLine(coldValue);
+            // }
+
+
+            //  List<string> stingList = new List<string>() { "Dolphins", "Dinosuars", "Bears", "Alligators", "Sloths" };
+
+            //    foreach (string allAnimals in stingList)//loop a string
+            //   {
+            //        Console.WriteLine(allAnimals);
+
+
+         //   List<bool> boolList = new List<bool>() { true, false, false, true, false };//bool foreach loop
+
+           // foreach (bool currentbool in boolList)
+            //{
+              //  if (currentbool == true)
+                //{
+                  //  Console.WriteLine("Better bring an umbrella");
+                //}
+                //else
+                //{
+                  //  Console.WriteLine("No pain today. Enjoy the rain");
+
+//                }
+
+               // int[] array1 = new int[] { 5, 2, 3, 4, 1, 5, 6, 3 };
+               // List<int> numberlist = new List<int>(array1);
+              //numberlist.Sort();
+              //  foreach(int orderedNumbers in numberlist)
+            //{
+                //Console.WriteLine(orderedNumbers);//this is a print of the numbers in order, but still in the list
+            }
+           // int[] arrayNew = numberlist.ToArray();********
+          //  Console.WriteLine(arrayNew[2]);//this will print spot 2 in array in new order
+
+        //    for (int i= 0; i<arrayNew.Length; i++)********
+            //{
+            //    array1[i] = arrayNew[i];
+             //   Console.WriteLine(array1[i]);//this brings it back into array1 and prints the loop with all numbers
+          //  }
+
+
+
+
+
+                //    Convert array1 to a List
+                //Use the.Sort() method on the list
+                //Convert the list back to an array and set it to the value for array1
+
+
+            }
+        }
+    
+
+
+internal class Student
+{
+   // public string StudentName { get; internal set; }
+  //  public int StudentsID { get; internal set; }
+}
+
